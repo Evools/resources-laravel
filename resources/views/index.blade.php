7994e7a776dd8a59dcebe97deb3f3604f62860f5
Commander <37365092+Evools@users.noreply.github.com>
2024-02-19 18:30:58 +0600

    cigrate db

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
-@section('title', 'Index')
+@section('title', 'Главная')
 
 @section('content')
     <div class="m-auto py-3 border-b border-slate-100 sticky top-0 bg-white">
         @include('layouts.nav')
     </div>
     <div class="flex items-start gap-12 mt-4 max-w-[1280px] m-auto">
-        <div class=" sticky top-20">
+        <div class=" sticky top-20 max-w-64">
             @include('layouts.category')
         </div>
         @include('include.cards')
--- resources/views/layouts/category.blade.php
+++ resources/views/layouts/category.blade.php
@@ -1,17 +1,10 @@
-<ul class="flex flex-col gap-4">
+<ul class="flex flex-col gap-4 w-[140px]">
     <li>
         <a href="/">Все</a>
     </li>
-    <li>
-        <a href="/">Иконки</a>
-    </li>
-    <li>
-        <a href="/">Иллюстрации</a>
-    </li>
-    <li>
-        <a href="/">Figma</a>
-    </li>
-    <li>
-        <a href="/">Шрифт</a>
-    </li>
+    @foreach ($categories as $category)
+        <li>
+            <a href="/">{{ $category->name }}</a>
+        </li>
+    @endforeach
 </ul>
