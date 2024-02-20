85431e3b53f4cc922743f3f1e66889d63ef2028b
Commander <37365092+Evools@users.noreply.github.com>
2024-02-20 08:32:53 +0600

    category components

--- resources/views/layouts/category.blade.php
+++ resources/views/layouts/category.blade.php
@@ -1,10 +1,11 @@
 <ul class="flex flex-col gap-4 w-[140px] text-stone-600">
     <li>
-        <a href="/" class="text-white">Все</a>
+        <a href="/" class="{{ request()->is('/') ? 'text-white' : '' }}">Все</a>
     </li>
     @foreach ($categories as $category)
         <li>
-            <a href="/category/{{ $category->id }}" class="hover:text-white transition">{{ $category->name }}</a>
+            <a href="/category/{{ $category->id }}"
+                class="{{ request()->is('category/' . $category->id) ? 'text-white' : '' }} hover:text-white transition">{{ $category->name }}</a>
         </li>
     @endforeach
 </ul>
